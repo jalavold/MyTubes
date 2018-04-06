ca2d5cc9dd8483f8770c9bdadc4fd19ed54acacc
git@github.com:jalavold/jalavold.github.io.git <j.aaron.lavold@gmail.com>
2018-04-06 16:18:29 -0700

    save

--- bingapicode.js
+++ bingapicode.js
@@ -29,7 +29,7 @@ function bingWebSearch(query, options, key) {
     request.setRequestHeader("Accept", "application/json");
 
     // event handler for successful response
-    //request.addEventListener("load", handleOnLoad);
+    request.addEventListener("load", handleOnLoad);
 
     // event handler for erorrs
     //request.addEventListener("error", function() {
@@ -63,4 +63,38 @@ function bingSearchOptions(form) {
 function searchBing() {
     var query = document.getElementById("query").value;
     return bingWebSearch(query, bingSearchOptions(), key);
+}
+
+function handleOnLoad() {
+    hideDivs("noresults");
+
+    var json = this.responseText.trim();
+    var jsobj = {};
+
+    // try to parse JSON results
+    try {
+        if (json.length) jsobj = JSON.parse(json);
+    } catch(e) {
+        renderErrorMessage("Invalid JSON response");
+    }
+
+    // show raw JSON and headers
+    document.getElementById("jsondump").innerHTML = formatwithpre(JSON.stringify(jsobj, null, 2));
+}
+
+
+// escape quotes to HTML entities for use in HTML tag attributes
+function escapeQuotes(text) {
+    return text.replace(/'/g, "&apos;").replace(/"/g, "&quot;");
+}
+
+// get the host portion of a URL, strpping out search result formatting and www too
+function getHost(url) {
+    return url.replace(/<\/?b>/g, "").replace(/^https?:\/\//, "").split("/")[0].replace(/^www\./, "");
+}
+
+// format plain text for display as an HTML <pre> element
+function formatwithpre(text) {
+    text = "" + text;
+    return "<pre>" + text.replace(/&/g, "&amp;").replace(/</g, "&lt;") + "</pre>"
 }
