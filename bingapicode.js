772ee8339fbf2fa120c06492d11d74031d0c0d25
git@github.com:jalavold/jalavold.github.io.git <j.aaron.lavold@gmail.com>
2018-04-07 00:37:06 -0700

    save

--- bingapicode.js
+++ bingapicode.js
@@ -87,6 +87,7 @@ function handleOnLoad() {
         var iframe = document.createElement("iframe");
         iframe.src = jsobj.value[video].contentUrl.replace("watch?v=","embed/");
         iframe.frameBorder = 0;
+        iframe.width = "40%";
         iframe.setAttribute("allow","autoplay");
         iframe.setAttribute("allowFullScreen","");
         document.getElementById("codeway").appendChild(iframe);
